aa1c5a112d79915c6f96b3d687abc525cd461d9b
Babiichuk Myroslava <102831246+DilemmaFixer3@users.noreply.github.com>
2023-05-14 20:30:43 +0300

    second commit

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -1,14 +1,25 @@
-CREATE TABLE flowers (
-    id     INT UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
-    field1 VARCHAR(200) NOT NULL
+CREATE TABLE cosmetic(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
+    name VARCHAR(150) NOT NULL ,
+    producer VARCHAR(100) NOT NULL ,
+    size INT NOT NULL ,
+    type_size VARCHAR(5) NOT NULL
 );
 
-INSERT INTO flowers
-    (field1)
+INSERT INTO cosmetic (id, name, producer, size, type_size)
 VALUES
-    ('Rose'),
-    ('Lotus'),
-    ('Jasmine'),
-    ('Sunflower'),
-    ('Daisy');
-
+    ('1', 'Cream', 'Garnier', '80','ML'),
+    ('2', 'Primer', 'Bielenda', '30','ML'),
+    ('3', 'Concealer', 'Maybelline New York', '6','ML'),
+    ('4', 'Tonal basis', 'Missha', '50','ML'),
+    ('5', 'Blash', 'Lamel Professional', '4','GR'),
+    ('6', 'Bronzer', 'Lamel Professional', '8','GR'),
+    ('7', 'Mascara', 'Maybelline New York', '9','ML'),
+    ('8', 'Eye shadow', 'Lamel Professional', '12','GR'),
+    ('9', 'Eyebrow gel', 'LOreal Paris', '3','GR'),
+    ('10', 'Eyebrow soap', 'Imagic', '10','GR'),
+    ('11', 'Eyeliner', 'NYX Professional Makeup', '1','ML'),
+    ('12', 'Shampoo', 'Esthetic House', '500','ML'),
+    ('13', 'Conditioner', 'Esthetic House', '500','ML'),
+    ('14', 'Hair mask', 'Brelil', '250','ML'),
+    ('15', 'Hair oil', 'CHI', '15','ML');
